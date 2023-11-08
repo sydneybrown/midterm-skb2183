01c6d3fc6f7a89d77d87c11efcccf2af00a1507f
sydneybrown <sydneykaylabrown@gmail.com>
2023-11-08 03:14:33 -0500

    adding notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
+}
+
+function createNote(note) {
+  return <Note key={note.id} title={note.title} content={note.content} />;
 }
 
 export default App;
